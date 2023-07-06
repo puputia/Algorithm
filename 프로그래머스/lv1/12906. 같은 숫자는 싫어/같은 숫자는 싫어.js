4b80e1de94f1ee490c8e02687a6e27d04fc6f272
Kanon <87532431+puputia@users.noreply.github.com>
2023-07-06 23:03:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.76 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr)
+{
+    var answer = [];
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+    
+    return arr.filter((val,index) => val != arr[index+1]);;
+}
