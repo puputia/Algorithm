81515a66f0d531fdf0828a504bd5cea75214d7cc
Kanon <87532431+puputia@users.noreply.github.com>
2023-07-12 01:52:39 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,6 @@
 function solution(nums) {
-    var answer = [];
-    var maxPocket =  nums.length / 2;
-        
-    for(let i = 0; i < nums.length; i++) {
-        if(answer.length < maxPocket) {
-            if(!answer.includes(nums[i])) {
-                answer.push(nums[i]);
-            }
-        }
-    }
-    
-    return answer.length;    
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)];
+
+  return arr.length > max ? max : arr.length
 }
