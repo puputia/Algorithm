9abe71ed26ddd46ef7e3fa1d6e38951917839912
Kanon <87532431+puputia@users.noreply.github.com>
2023-07-21 21:02:15 +0900

    [level 1] Title: 최소직사각형, Time: 6.85 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,12 @@
+function solution(sizes) {
+    let maxWidth = 0, maxHeight = 0;
+
+    sizes.forEach((size) => {
+        size.sort((a, b) => a - b);
+        const [width, height] = size;
+        if (maxWidth < width) maxWidth = width;
+        if (maxHeight < height) maxHeight = height;
+    });
+    
+    return maxWidth * maxHeight;
+}
