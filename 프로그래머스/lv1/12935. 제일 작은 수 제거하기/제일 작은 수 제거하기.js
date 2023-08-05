1347231c197f46b0ac7c308bc98220f575351925
Kanon <87532431+puputia@users.noreply.github.com>
2023-08-05 15:23:44 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    if (arr.length === 1) return [-1];
+    
+    let i = 0;
+    for (let j = 0; j < arr.length; j += 1) {
+        if (arr[i] > arr[j]) i = j;
+    }
+    arr.splice(i, 1);
+    return arr;
+}
