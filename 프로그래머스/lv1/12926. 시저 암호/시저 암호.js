36c8350403ce8b0c1a30b3c7382d870de80032ba
Kanon <87532431+puputia@users.noreply.github.com>
2023-08-06 22:01:47 +0900

    [level 1] Title: 시저 암호, Time: 26.00 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,27 @@
+function solution(s, n) {
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  let answer = '';
+
+  for (let i = 0; i < s.length; i++) {
+    const str = s[i];
+    // 문자가 아닌 공백일 경우 덧셈 할당
+    if (str == ' ') {
+      answer += ' ';
+      continue;
+    }
+
+    // 대문자 문자열에 해당하는 문자가 있을 시 upper, 없다면 lower 할당
+    const upperOrLower = upper.includes(str) ? upper : lower;
+    // 할당한 문자열에 매개변수 s에 해당하는 인덱스와 n 더하기
+    let index = upperOrLower.indexOf(str) + n;
+    // 만약 더한 인덱스가 할당한 문자열의 길이보다 길거나 같을 경우 인덱스 - 문자열 길이 빼기
+    if (index >= upperOrLower.length) {
+      index -= upperOrLower.length;
+    }
+    // 계산한 인덱스에 해당하는 문자 덧셈 할당
+    answer += upperOrLower[index];
+  }
+
+  return answer;
+}
