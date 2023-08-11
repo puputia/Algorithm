fbc3a1c92f3c7b27a698afd381c3621c9082d320
Kanon <87532431+puputia@users.noreply.github.com>
2023-08-12 02:27:06 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,9 +1,5 @@
 function solution(seoul) {
-    var answer = '';
     let x = seoul.indexOf("Kim");
-   
-            answer = "김서방은 "+ x + "에 있다";
-    
-   
-   return answer;
+    var answer = "김서방은 " + x + "에 있다";
+    return answer;
 }
