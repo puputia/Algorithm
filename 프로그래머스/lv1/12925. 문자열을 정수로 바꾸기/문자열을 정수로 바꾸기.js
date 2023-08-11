944f96ff90562000d869a3051bce83a568273c5a
Kanon <87532431+puputia@users.noreply.github.com>
2023-08-12 02:20:35 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,11 +1,4 @@
 function solution(s) {
-    var answer = 0;
-    if(s >= 0) {
-        answer = s;
-    }
-    else {
-        answer = -1 * -1 * s;
-    }
-    
-    return parseFloat(answer);
+    var answer = parseInt(s);
+    return answer;
 }
