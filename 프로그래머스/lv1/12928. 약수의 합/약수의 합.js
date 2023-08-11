7916b979aee0e7861f705422d7e9d08554bf3c6a
Kanon <87532431+puputia@users.noreply.github.com>
2023-08-12 02:18:00 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,16 +1,15 @@
 function solution(n) {
-    var array = [];
-    for (let i = 1; i <= n; i++ ) {
-        if (n % i === 0) {
-            array.push(i);
+    let array = [];
+    for(let i =1; i<=n; i++) {
+        if(n%i ===0) {
+            array.push(i) 
         }
     }
-
+    
     let sum = 0;
-
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-    }
+    for(let i=0; i<array.length; i++) {
+        sum += array[i] 
+}
     
     return sum;
 }
