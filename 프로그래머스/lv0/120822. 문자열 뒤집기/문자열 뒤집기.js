d978c8aea2570d89763707cfb1c7e3f9c079acf5
Kanon <87532431+puputia@users.noreply.github.com>
2023-08-12 09:40:19 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,8 +1,4 @@
 function solution(my_string) {
-    
-    var answer = '';
-    var splitArray = my_string.split("");
-    var reverseArray = splitArray.reverse();
-    var answer = reverseArray.join("");
+    var answer = my_string.split("").reverse().join("");
     return answer;
 }
