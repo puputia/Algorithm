297f6f6d8269dcbc37ca688039485870ccdc5cd8
Kanon <87532431+puputia@users.noreply.github.com>
2023-08-12 09:12:09 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,9 +1,10 @@
 function solution(numbers) {
-  
-    var sum = 0;
+    var answer = 0;
+    let sum = 0;
     for(i=0; i<numbers.length; i++) {
         sum += numbers[i];
     }
-    var answer = sum/numbers.length;
-    return answer;
+    
+
+    return answer = sum/numbers.length;
 }
