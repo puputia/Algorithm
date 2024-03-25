62da321c4a3d5106681dffbb9fb351057c40182b
Kanon <87532431+puputia@users.noreply.github.com>
2024-03-25 16:07:58 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -0,0 +1,10 @@
+function solution(number, n, m) {
+    let answer = 0;
+    if(number%n==0 && number%m==0) {
+        answer = 1;
+    }
+    else {
+        answer = 0;
+    }
+    return answer;
+}
